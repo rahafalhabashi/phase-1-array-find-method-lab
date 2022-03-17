593bcb034e142900afc2b99e52f63c7d4d2eb30f
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-16 23:08:07 -0400

    commit tree d903c119c4199232c57856ce7d1c3acc0d2d7c6a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(year => year.result === "W")
+    console.log(result)
+    if (result) {
+        return result.year;
+    }    
+    else {
+         return undefined;
+    }
+}
